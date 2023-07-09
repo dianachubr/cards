4827033925dda14b1b5ee57c505b355a14e357d4
Diana Chubrina <dianachubrina@gmail.com>
2023-07-09 20:03:11 +0200

    message

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 const isProduction = process.env.MODE_ENV === "production";
 
 module.exports = {
   entry: "./js/index.js",
+  //mode: "development",
   mode: isProduction ? "production" : "development",
   module: {
     rules: [
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.css$/, use: [MiniCssExtractPlugin, "css-loader"] },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
@@ -20,12 +23,19 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
+    ],
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true,
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new CopyPlugin({
       patterns: [
         {
@@ -39,7 +49,9 @@ module.exports = {
       ],
     }),
     new HtmlWebpackPlugin({
+      filename: "game.html",
       template: "./game.html",
     }),
   ],
+  devtool: isProduction ? "hidden-source-map" : "source-map",
 };
